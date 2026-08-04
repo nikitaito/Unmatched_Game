eb817fe8fdde6df6d2f6e96a547b42bb6e26fd21
Nikan <nik2006majid@gmail.com>
2026-08-04 13:01:07 +0330

    Add structure for the button

--- include/front/main_menu.h
+++ include/front/main_menu.h
@@ -4,6 +4,18 @@
 
 #include "raylib.h"
 
+struct MenuButton
+{
+    const char* text;
+    int fontSize;
+    Color idleBg;
+    Color hoverBg;
+    Color pressBg;
+    Color idleBorder;
+    Color hoverBorder;
+    Color idleText;
+    Color hoverText;
+};
 
 class menu
 {
