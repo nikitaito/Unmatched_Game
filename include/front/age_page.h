34f61029c58f1c529b0b7e69fe1db0dfb90c0f54
Nikan <nik2006majid@gmail.com>
2026-08-05 12:05:19 +0330

    Add DrawMenuButton to age_page class

--- include/front/age_page.h
+++ include/front/age_page.h
@@ -5,12 +5,23 @@
 #include <string.h>
 #include "raylib.h"
 
-
+struct MenuButton
+{
+    const char* text;
+    int fontSize;
+    Color idleBg;
+    Color hoverBg;
+    Color pressBg;
+    Color idleBorder;
+    Color hoverBorder;
+    Color idleText;
+    Color hoverText;
+};
 
 class AgePage
 {
     private:
-
+        bool DrawMenuButton(MenuButton& , Rectangle , Font ,Vector2 , bool);
     public:
         
 };
