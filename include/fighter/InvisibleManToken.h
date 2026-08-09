9f63e386eb775cd21dd8673676d6f33979eea576
Nikan <nik2006majid@gmail.com>
2026-08-09 16:36:04 +0330

    creat invman token inheritance of sidekick class

--- include/fighter/InvisibleManToken.h
+++ include/fighter/InvisibleManToken.h
@@ -9,9 +9,6 @@ class InvisibleManToken : public Sidekick
         ~InvisibleManToken() override = default;
 
         bool occupies_space() const override;
-
-        // this token has no life the way Dr_Watson/Sister do: it can't be
-        // removed from the board by taking damage
         bool get_islive() const override;
         void Damage(int) override;
 };
