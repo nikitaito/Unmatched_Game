c0efe992e345173934bca75593cd20d3e09cdcee
Nikan <nik2006majid@gmail.com>
2026-08-22 19:16:10 +0330

    Adding Covert_PreParationEffect to Covert_PreParation card

--- src/fighter/Invisibleman.cpp
+++ src/fighter/Invisibleman.cpp
@@ -16,7 +16,9 @@ void Invman :: initial_deck() {
     cards.deck.push_back(Card(CardName :: ConFound , CardTiming :: After , CardType :: BOTH , Usertype :: INVMAN , 3 , 3 , 2 , Make_Effect({new ConfoundEffect()})));
     cards.deck.push_back(Card(CardName :: ConFound , CardTiming :: After , CardType :: BOTH , Usertype :: INVMAN , 3 , 3 , 2 , Make_Effect({new ConfoundEffect()})));
 
-
+    cards.deck.push_back(Card(CardName :: Covert_PreParation , CardTiming :: After , CardType :: BOTH , Usertype :: INVMAN , 2, 2 , 1 , Make_Effect({new CovertPreparationEffect()})));
+    cards.deck.push_back(Card(CardName :: Covert_PreParation , CardTiming :: After , CardType :: BOTH , Usertype :: INVMAN , 2, 2 , 1 , Make_Effect({new CovertPreparationEffect()})));
+    cards.deck.push_back(Card(CardName :: Covert_PreParation , CardTiming :: After , CardType :: BOTH , Usertype :: INVMAN , 2, 2 , 1 , Make_Effect({new CovertPreparationEffect()})));
 
 
     cards.deck.push_back(Card(CardName :: Deaming_of_Revenge , CardTiming :: After , CardType :: BOTH , Usertype :: INVMAN , 3, 3 , 1 , Make_Effect({new DamageIfOnFog(1)})));
